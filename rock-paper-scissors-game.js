9f0c68ff461155e8b2baa67b942e40b2613152f2
Ellie <101657425+elliehl@users.noreply.github.com>
2022-09-10 00:46:00 +0100

    Add win, loss or draw conditions

--- rock-paper-scissors-game.js
+++ rock-paper-scissors-game.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-  const computerOptions = ["Rock", "Paper", "Scissors"];
+  const computerOptions = ["rock", "paper", "scissors"];
   let computerChoice = "";
   computerChoice +=
     computerOptions[Math.floor(Math.random() * computerOptions.length)];
@@ -7,3 +7,33 @@ function getComputerChoice() {
 }
 
 getComputerChoice();
+
+function playSingleRound(playerChoice, computerChoice) {
+  if (playerChoice.toLowerCase() === "rock") {
+    if (computerChoice === "scissors") {
+      return "You win!";
+    } else if (computerChoice === "paper") {
+      return "Sorry, you lose this time";
+    } else return "It's a draw";
+  }
+
+  if (playerChoice.toLowerCase() === "paper") {
+    if (computerChoice === "rock") {
+      return "You win!";
+    } else if (computerChoice === "scissors") {
+      return "Sorry, you lose this time";
+    } else return "It's a draw";
+  }
+
+  if (playerChoice.toLowerCase() === "scissors") {
+    if (computerChoice === "paper") {
+      return "You win!";
+    } else if (computerChoice === "rock") {
+      return "Sorry, you lose this time";
+    } else return "It's a draw";
+  }
+}
+
+let computerScore = 0;
+let playerScore = 0;
+function game() {}
